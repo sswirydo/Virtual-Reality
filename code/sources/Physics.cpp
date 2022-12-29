06fb52f70370870c27fcd719963d278252014252
pap sanou <pap.sanou@ulb.be>
2022-12-29 04:40:59 +0100

    add borne collision shape

--- code/sources/Physics.cpp
+++ code/sources/Physics.cpp
@@ -23,12 +23,26 @@ Physics::Physics()
     {
         //Creates the ground shape
         btCollisionShape* groundShape = new btBoxShape(btVector3(107, 1, 100));
+        btCollisionShape* borneShape_l = new btBoxShape(btVector3(0.5, 1, 200));
+        btCollisionShape* borneShape_r = new btBoxShape(btVector3(0.5, 1, 200));
+
+        btDefaultMotionState* borneShape_l_ms = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(-7, 0, 0)));
+        btDefaultMotionState* borneShape_r_ms = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(7, 0, 0)));
+        
+        btRigidBody::btRigidBodyConstructionInfo borne_l_RigidBodyCI(0, borneShape_l_ms, borneShape_l);
+        btRigidBody::btRigidBodyConstructionInfo borne_r_RigidBodyCI(0, borneShape_r_ms, borneShape_r);
+        btRigidBody *rigidBody_l = new btRigidBody(borne_l_RigidBodyCI);
+        btRigidBody *rigidBody_r = new btRigidBody(borne_r_RigidBodyCI);
         //Stores on an array for reusing
         collisionShapes.push_back(groundShape);
+        collisionShapes.push_back(borneShape_l);
+        collisionShapes.push_back(borneShape_r);
         //Creates the ground rigidbody
         btRigidBody* groundRigidBody = createGroundRigidBodyFromShape(groundShape);
         //Adds it to the world
         this->world->addRigidBody(groundRigidBody);
+        this->world->addRigidBody(rigidBody_l);
+        this->world->addRigidBody(rigidBody_r);
     }
 
 }
