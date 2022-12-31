47e7d29f67a8ea4c12ce658eda90c17645bde187
sswirydo <szymsimon@gmail.com>
2022-12-31 05:47:19 +0100

    [ADD] Base for Sound class

---
+++ code/headers/Sound.hpp
@@ -0,0 +1,17 @@
+#ifndef SOUND_HPP
+#define SOUND_HPP
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include "AL/alext.h"
+
+#include "sndfile.h"
+
+class Sound 
+{
+public:
+	Sound();
+};
+
+
+#endif //! SOUND_HPP
---
+++ code/sources/Sound.cpp
@@ -0,0 +1,2 @@
+#include "../headers/Sound.hpp"
+
