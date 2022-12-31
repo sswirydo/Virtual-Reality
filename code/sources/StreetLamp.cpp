7a3e965d03eceada224a3d880b0914b6eaa369d2
pap sanou <pap.sanou@ulb.be>
2022-12-31 18:43:44 +0100

    amelioration des couleurs + lumieres

--- code/sources/StreetLamp.cpp
+++ code/sources/StreetLamp.cpp
@@ -6,7 +6,7 @@ StreetLamp::StreetLamp(Model* model, Shader* shader, Physics* physics, LightSour
     //this->setModelMatrix(glm::scale(this->getModelMatrix(),glm::vec3(6.0f)));
     this->isAtRight = isAtRight;
     glm::vec3 myLightPosition; 
-    this->my_light = LightSource(glm::vec3(0.0f),glm::vec4(0.9f,0.7f,0.1f,0.3f));
+    this->my_light = LightSource(glm::vec3(0.0f),glm::vec4(1.0f,1.0f,1.0f,0.3f));
     if(this->isAtRight){
         this->translateModel(glm::vec3(9.f,0.0f,-position* LAMP_SPACING));
         myLightPosition = glm::vec3(this->getModelMatrix()[3]) + glm::vec3(-3.8,8.3,0);
@@ -29,7 +29,7 @@ void StreetLamp::render(Camera* camera, std::vector<StreetLamp*> lamps)
     else
         myLightPosition = glm::vec3(this->getModelMatrix()[3])+ glm::vec3(3.8,8.3,0);
     this->my_light.setPosition(myLightPosition);
-    this->my_light.show(camera);
+    // this->my_light.show(camera);
 }
 
 StreetLamp::~StreetLamp()
