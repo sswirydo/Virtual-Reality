37c5ebedf944106e9dff3963acbec3cfa6acd13f
pap sanou <pap.sanou@ulb.be>
2023-01-07 08:38:36 +0100

    sound added only on linux for the moment

--- code/headers/Sound.hpp
+++ code/headers/Sound.hpp
@@ -8,13 +8,8 @@
 //#include <sndfile.h>
 #include <irrKlang.h>
 #include <vector>
-#include <inttypes.h>
 #include <iostream>
 using namespace irrklang;
-
-// To be able to use the irrKlang.dll file, we need to link with the irrKlang.lib.
-// We could set this option in the project settings, but to make it easy we use
-// a pragma comment:
 
 #pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
 class Sound
